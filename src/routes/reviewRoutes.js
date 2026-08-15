96e51da854ec485cba3e1be3f6b0c63732a39edb
Elamkavi <elamkavi4@gmail.com>
2026-08-15 12:11:33 +0530

    Delete src directory

--- src/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authenticate } = require('../middleware/auth');
-const ctrl = require('../controllers/reviewController');
-
-const router = express.Router();
-router.get('/store/:storeId', ctrl.listForStore); // public
-router.post('/', authenticate, ctrl.create);
-
-module.exports = router;
